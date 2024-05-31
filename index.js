45df47b3872b1a4e8a496ef6d8618d770e9fd70e
MartaBlancobe <martablancobe@gmail.com>
2024-05-31 02:10:30 +0200

    fix and test-conection

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config();
+
 
 const db = require('./config/db');
 const indexRouter = require('./routes/index');
